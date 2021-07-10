6417ffb3103e1f0d713bd1d456875544a6f9a79d
Daniele Gaspariam <danigaspariam@gmail.com>
2021-07-10 20:59:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 
-async function getContent() {
-    try{
-        const response = await fetch('http://localhost:3030/');
-        const data = await response.json();
-        console.log(data);
-    }
-    catch (error){
-        console.error(error);
-    }
+async function somaDeNumeros() {
+
+    const response = await fetch('http://localhost:3030/');
+    const data = await response.json();
+    const inteiro = document.getElementById('numbers');
+
+    let somar = data.push(inteiro);
+    document.getElementById('size').innerHTML + somar; 
+    
 }
-getContent()
+
+somaDeNumeros()
 
 
 
